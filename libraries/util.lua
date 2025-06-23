7786e7c6f4c46eaeb4f1d0090e4c7add71e620ef
Zvbhrf <yamirka.su@gmail.com>
2025-06-23 18:17:23 +0200

    Deprecate some util functions

--- libraries/util.lua
+++ libraries/util.lua
@@ -5,14 +5,16 @@ util = util or {}
 ---Calls function on `GM:Initialize`.
 ---@param func function
 ---@param priority? boolean
----
+---@deprecated Use "hooks.Initialize:Once" instead.
+---@see hooks
 ---@shared
 function util.OnInitialize(func, priority) end
 function util.OnInitialise(func, priority) end
 
 ---Calls function in the upcoming tick.
 ---@param func function
----
+---@deprecated Use "timer.Simple(0, function)" instead.
+---@see hooks
 ---@shared
 function util.NextTick(func) end
 function util.NextThink(func) end
@@ -20,7 +22,8 @@ function util.NextThink(func) end
 ---Calls function on `GM:InitPostEntity`.
 ---@param func function
 ---@param priority? boolean
----
+---@deprecated Use "hooks.InitPostEntity:Once" instead.
+---@see hooks
 ---@shared
 function util.OnInitPostEntity(func, priority) end
 function util.OnEntityInitialize(func, priority) end
@@ -30,24 +33,24 @@ function util.OnEntityInitialise(func, priority) end
 ---Useful to create/render materials(RTs), setup fonts, check settings, etc.
 ---@param func function
 ---@param priority? boolean
----
+---@deprecated Use "hooks.PostRender:Once" instead.
+---@see hooks
 ---@shared
 function util.OnFirstFrame(func, priority) end
 
 ---Returns player's spawn time (CurTime())
 ---@return number spawnTime
----
+---@deprecated Use "hooks.Initialize:Once" instead.
+---@see hooks
 ---@shared
 function PLAYER:GetSpawnTime() end
 
 ---Returns player's coins
 ---@return number coins
----
 ---@shared
 function PLAYER:GetCoins() end
 
 ---Sets N coins for player
 ---@param coins number
----
 ---@server
 function PLAYER:SetCoins(coins) end
